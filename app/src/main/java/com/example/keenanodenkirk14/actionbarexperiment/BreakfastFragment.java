b602ad9a253d61caf9538d5630030b7065a0e26e
keenanodenkirk14 <Giantpanda7>
2018-03-19 16:26:13 -0500

    Formats all fragment xml's

--- app/src/main/java/com/example/keenanodenkirk14/actionbarexperiment/BreakfastFragment.java
+++ app/src/main/java/com/example/keenanodenkirk14/actionbarexperiment/BreakfastFragment.java
@@ -4,5 +4,14 @@ package com.example.keenanodenkirk14.actionbarexperiment;
  * Created by keenanodenkirk14 on 3/19/2018.
  */
 
-public class BreakfastFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BreakfastFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_breakfast,container,false);
+    }
 }
--- app/src/main/java/com/example/keenanodenkirk14/actionbarexperiment/LunchFragment.java
+++ app/src/main/java/com/example/keenanodenkirk14/actionbarexperiment/LunchFragment.java
@@ -1,8 +1,17 @@
 package com.example.keenanodenkirk14.actionbarexperiment;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by keenanodenkirk14 on 3/19/2018.
  */
 
-public class LunchFragment {
+public class LunchFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_lunch,container,false);
+    }
 }
