7b95615a317f8e53576ef77f8cb1a0f5ea018be1
keenanodenkirk14 <Giantpanda7>
2018-03-21 15:12:50 -0500

    Edits my.xml to include remainder of book code

--- app/src/main/java/com/example/keenanodenkirk14/actionbarexperiment/MainActivity.java
+++ app/src/main/java/com/example/keenanodenkirk14/actionbarexperiment/MainActivity.java
@@ -80,10 +80,10 @@ public class MainActivity extends AppCompatActivity {
 
         public boolean onOptionsItemSelected(MenuItem item){
             int id = item.getItemId();
-            if(id == R.id.){
+            if(id == R.id.action_settings){
                 return true;
             }
-            return super.onOptionsItemSelected(item);
+            return true;
         }
     }
 }
